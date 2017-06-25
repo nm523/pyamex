6bc268c5f36755349adad70142d468da25450a8d
niall <nm523@cam.ac.uk>
2017-06-25 23:54:00 +0100

    Fixed installation

--- build/lib/pyamex/__init__.py
+++
@@ -1,5 +0,0 @@
-from .card import CardAccount
-from .client import AmexClient
-from .version import __version__
-from .transaction import Transaction
-from .loyalty import LoyaltyProgramme
--- build/lib/pyamex/loyalty.py
+++
@@ -1,7 +0,0 @@
-class LoyaltyProgramme:
-    """
-    Description and points balance for a loyalty programme
-    """
-    def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
