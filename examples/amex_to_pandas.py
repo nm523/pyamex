1d4268dde5e9f2cff729f8b21d34117c82f56647
Niall <mcconville.m.niall@pwc.com>
2019-04-13 15:57:40 +0100

    Formatting changes to example

--- examples/amex_to_pandas.py
+++ examples/amex_to_pandas.py
@@ -9,9 +9,9 @@ import calendar
 import pandas as pd
 
 login = {
-	'username': input('Username: '),
-	'password': getpass.getpass('Password: '),
-	'locale': 'en_GB'
+    'username': input('Username: '),
+    'password': getpass.getpass('Password: '),
+    'locale': 'en_GB'
 }
 
 # Select the number of billing periods
@@ -25,17 +25,17 @@ accounts = client.accounts()
 
 result = []
 for account in accounts:
-	billing_periods = account.transactions(periods)
-	for _, transactions in billing_periods.items():
-		for transaction in transactions:
-			result.append(
-					(account.card_product,
-					 account.lending_type,
-					 account.card_number_suffix,
-					 transaction.date, 
-					 transaction.narrative,
-					 transaction.trans_amount)
-				)
+    billing_periods = account.transactions(periods)
+    for _, transactions in billing_periods.items():
+        for transaction in transactions:
+            result.append(
+                (account.card_product,
+                 account.lending_type,
+                 account.card_number_suffix,
+                 transaction.date,
+                 transaction.narrative,
+                 transaction.trans_amount)
+            )
 
 df = pd.DataFrame(result)
 df.columns = ['CardProduct', 'LendingType',
@@ -43,11 +43,11 @@ df.columns = ['CardProduct', 'LendingType',
               'Description', 'Amount']
 
 df['DayOfWeek'] = df.Date \
-                    .map(lambda date : date.weekday())
+    .map(lambda date: date.weekday())
 
 summary = df[(df.Amount > 0)].groupby('DayOfWeek') \
-                             .agg(['mean', 'std', 'min', 'max'])
+    .agg(['mean', 'std', 'min', 'max'])
 
 # Human readable + sorted
-summary.index = summary.index.map(lambda day : calendar.day_name[day])
+summary.index = summary.index.map(lambda day: calendar.day_name[day])
 print(summary)
